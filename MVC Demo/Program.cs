2a62f102c2b5f4cd3585f3c39a16556465285551
Alexander Gliedman-Adler <alexgliedmanadler@gmail.com>
2015-04-24 12:43:17 -0400

    Demo Blog Creation Main()

---
+++ MVC Demo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BloggingContext())
+            {
+                //Create and save a new Blog
+                Console.Write("Enter a name for a new Blog: ");
+                var name = Console.ReadLine();
+
+                var blog = new Blog { Name = name };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+
+                //Display all blogs from the db
+                var query = from b in db.Blogs
+                            orderby b.Name
+                            select b;
+
+                Console.WriteLine("All blogs inthe database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
